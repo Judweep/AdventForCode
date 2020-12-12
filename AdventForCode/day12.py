61c09d0039d94a42ff350ce35295bea4dc9dd070
Judweep <judith.stecklina@gmx.de>
2020-12-13 00:25:06 +0200

    Completed part 2

--- AdventForCode/day12.py
+++ AdventForCode/day12.py
@@ -15,45 +15,53 @@ with open("C:\\Users\\Judith\\Documents\\AdventForCode\\day12_input.txt", "r") a
 
 class ship():
     def __init__(self, location, direction):
-        self.v, self.h = location
-        #self.location = [self.v, self.h]
-        self.direction = direction
+        self.pos_v, self.pos_h = location
+        #self.location = [self.pos_v, self.pos_h]
+        self.dir_v, self.dir_h = direction
     
     def move(self, action, value):
         if action == "N":
-            self.v += value
+            self.dir_v += value
         elif action == "E":
-            self.h += value
+            self.dir_h += value
         elif action == "S":
-            self.v -= value
+            self.dir_v -= value
         elif action == "W":
-            self.h -= value
+            self.dir_h -= value
         elif action == "L":
-            self.direction = (self.direction - int(value / 90)) % 4
-        elif action == "R":
-            self.direction = (self.direction + int(value / 90)) % 4
-        elif action == "F":
-            if self.direction == 0:
-                self.v += value
-            elif self.direction == 1:
-                self.h += value
-            elif self.direction == 2:
-                self.v -= value
-            elif self.direction == 3:
-                self.h -= value
+            angle = int(value / 90)
+            if angle == 1:
+                self.dir_v, self.dir_h = self.dir_h, -self.dir_v
+            elif angle ==2:
+                self.dir_v, self.dir_h = -self.dir_v, -self.dir_h
+            elif angle ==3:
+                self.dir_v, self.dir_h = -self.dir_h, self.dir_v
             else:
                 print("Error")
+        elif action == "R":
+            angle = int(value / 90)
+            if angle == 1:
+                self.dir_v, self.dir_h = -self.dir_h, self.dir_v
+            elif angle ==2:
+                self.dir_v, self.dir_h = -self.dir_v, -self.dir_h
+            elif angle ==3:
+                self.dir_v, self.dir_h = self.dir_h, -self.dir_v
+            else:
+                print("Error")
+        elif action == "F":
+            self.pos_v += value * self.dir_v
+            self.pos_h += value * self.dir_h
         else:
             print("Error")
     def locate(self, distance = True):
         if distance:
-            return abs(self.v) + abs(self.h)
+            return abs(self.pos_v) + abs(self.pos_h)
         else:
-            return (self.v, self.h)
+            return (self.pos_v, self.pos_h)
         
             
 
-ferry = ship([0,0], 1)
+ferry = ship([0,0], [1,10])
 for action, value in inputlst:
     ferry.move(action, value)
 print(f"Ferry is here: {ferry.locate()} and moved {ferry.locate(distance = False)} units")
