f1a86cfc2bc1b1ff8d63eb7f3e76fb2212da0f7e
Judweep <judith.stecklina@gmx.de>
2021-12-03 00:47:07 +0100

    Deleted all files

--- AdventForCode/day8.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  9 14:34:25 2020
-
-@author: Judith
-"""
-
-with open("C:\\Users\\Judith\\Documents\\AdventForCode\\day9_input.txt", "r") as file:
-    input = file.readlines()
